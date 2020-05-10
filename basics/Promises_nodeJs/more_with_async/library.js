a485f2b422d66a3cd1f301683b0ff060e322dc5c
gajanangitte <gajanan_s@cs.iitr.ac.in>
2020-05-10 09:39:37 +0530

    Added step3 of cooking promises! with async and await

--- basics/Promises_nodeJs/more_with_async/library.js
+++ basics/Promises_nodeJs/more_with_async/library.js
@@ -1,5 +1,9 @@
 /*
-this is the brainstormDinner function. It's a little silly. It returns a promise that uses a series of setTimeout() functions to simulate a time-consuming asynchronous action. It's a good example of "callback hell" or "the pyramid of doom," two ways people describe how confusing a bunch of nested callback functions can become.
+this is the brainstormDinner function. It's a little silly. 
+It returns a promise that uses a series of setTimeout() functions to 
+simulate a time-consuming asynchronous action. It's a good example of
+"callback hell" or "the pyramid of doom," two ways people describe how
+ confusing a bunch of nested callback functions can become.
 */
 
 const brainstormDinner = () => {
