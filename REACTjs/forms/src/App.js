b4b47465e3681aa9622ccebf96b20c835e0f4a4b
gajanangitte <gajanan_s@cs.iitr.ac.in>
2020-05-23 17:45:10 +0530

    Changed the forms

--- REACTjs/forms/src/App.js
+++ REACTjs/forms/src/App.js
@@ -1,21 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-class App extends Component {
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: 'ihatehumans',
+      authorized: false
+    };
+    this.authorize = this.authorize.bind(this);
+  }
+
+  authorize(e) {
+    const password = e.target.querySelector(
+      'input[type="password"]').value;
+    const auth = password == this.state.password;
+    this.setState({
+      authorized: auth
+    });
+  }
+
   render() {
+    const login = (
+  <form action="#" onSubmit={this.authorize}>
+    <input 
+      type="password" 
+      placeholder="Password" />
+    <input type="submit" />
+  </form>
+    );
+
+  const contactInfo = (
+     <ul>
+          <li>
+            client@example.com
+          </li>
+          <li>
+            555.555.5555
+          </li>
+      </ul>
+    );
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="authorization">
+        <h1>  { this.state.authorized ? 'Contact' : 'Enter the Password' }</h1>
+          { this.state.authorized ? contactInfo : login }
       </div>
     );
   }
 }
 
-export default App;
+export default Contact;
