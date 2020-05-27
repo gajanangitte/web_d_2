99135f8c100d3e9fe6a4aa7315ab85493b220f81
gajanangitte <gajanan_s@cs.iitr.ac.in>
2020-05-27 10:26:22 +0530

    Added new React based color generator project

---
+++ REACTjs/Color Picker/color_generator/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Random from './App';
+import './index.css';
+
+ReactDOM.render(
+  <Random />,
+  document.getElementById('root')
+);
