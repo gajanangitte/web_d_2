1c32453b8c1ab008afc1d481d0f9a80cfd13ae7d
gajanangitte <gajanan_s@cs.iitr.ac.in>
2020-08-29 18:16:38 +0530

    GoogleAuth Added

--- TwitchMockUp/client/src/components/header.js
+++ TwitchMockUp/client/src/components/header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import GoogleAuth from './GoogleAuth'
 
 
 function Header() {
@@ -14,6 +15,8 @@ function Header() {
                 <Link to='/' className='item'>
                     All Steams
                 </Link>
+
+                <GoogleAuth />
 
             </div>
         </div>
